74e809976bd451c305a6c896bdb29c4a8a5d0467
bebe <benish.khan@netlify.com>
2022-07-21 00:08:07 -0700

    Add url to main page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Box with Daniel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Box with Daniel" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Checkout Yelp here{" "}
+          <code>https://www.yelp.com/biz/box-with-daniel-san-francisco</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
