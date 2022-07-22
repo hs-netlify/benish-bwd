823b7acf8870f780006f8a04de13ab8a722de78b
Henry Smith <henry.smith@netlify.com>
2022-07-22 13:42:09 +0100

    CSS updates, images and tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,80 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-import bwd from "../public/bwd.svg";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="flex flex-col justify-between h-screen w-full">
       <Head>
         <title>Box with Daniel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div></div>
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="w-full h-1"></div>
 
-      <main>
-        <Header title="Box with Daniel" />
-        <body>
-          <p>
+        <img className="w-72" src="/bwd.svg" />
+        <div className="flex flex-col items-center mt-10">
+          <code className="p-2">
             <b>Phone number: 415-309-5378</b>
-          </p>
-          <p>
-            <b>Email: info@boxwithdaniel.com</b>
-          </p>
-          <code>Website: www.boxwithdaniel.com</code>
-        </body>
-
-        <p className="description">
-          Yelp{" "}
-          <code>https://www.yelp.com/biz/box-with-daniel-san-francisco</code>
-        </p>
-        <p className="description">
-          Instagram
-          <code>https://www.instagram.com/boxwithdaniel/</code>
-        </p>
-        <p className="description">
-          TikTok <code>https://www.tiktok.com/@boxwithdaniel</code>
-        </p>
-        <p className="description">
-          Book Time with me: <code>https://calendly.com/boxwithdaniel</code>
-        </p>
-      </main>
+          </code>
+          <code className="p-2">
+            <b>
+              mail:
+              <a
+                className="cursor-pointer hover:text-blue-700 underline"
+                href="mailto:info@boxwithdaniel.com"
+              >
+                info@boxwithdaniel.com
+              </a>
+            </b>
+          </code>
+          <code className="p-2">
+            Website:
+            <a
+              className="cursor-pointer hover:text-blue-700 underline"
+              target="blank"
+              href="www.boxwithdaniel.com"
+            >
+              www.boxwithdaniel.com
+            </a>
+          </code>
+        </div>
+        <div className="flex mt-10">
+          <div className="p-2">
+            <a href="https://www.yelp.com/biz/box-with-daniel-san-francisco">
+              <img
+                className="hover:scale-110 duration-200 ease-in-out transition-all w-8"
+                src="/icons/yelp.svg"
+              />
+            </a>
+          </div>
+          <div className="p-2">
+            <a href="https://www.instagram.com/boxwithdaniel/">
+              <img
+                className="hover:scale-110 duration-200 ease-in-out transition-all w-8"
+                src="/icons/instagram.svg"
+              />
+            </a>
+          </div>
+          <div className="p-2">
+            <a href="https://www.tiktok.com/@boxwithdaniel">
+              <img
+                className="hover:scale-110 duration-200 ease-in-out transition-all w-8"
+                src="/icons/tiktok.svg"
+              />
+            </a>
+          </div>
+          <div className="p-2">
+            <a href="https://calendly.com/boxwithdaniel">
+              <img
+                className="hover:scale-110 duration-200 ease-in-out transition-all w-8"
+                src="/icons/clock.svg"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
