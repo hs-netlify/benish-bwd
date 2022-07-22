836135426ce76e422e0f0dd08900994d11dec875
bebe <benish.khan@netlify.com>
2022-07-21 21:53:55 -0700

    Update img tag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import bwd from "/Users/bebe/box-with-daniel/public/bwd.svg";
 
 export default function Home() {
   return (
